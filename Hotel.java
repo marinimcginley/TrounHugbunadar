bd18660345844065ac33505549729447d6868a7b
magdadianaa <magdadianaa@gmail.com>
2018-03-27 16:34:09 +0000

    pushaði óvart tóma hotel skjali

--- Hotel.java
+++ Hotel.java
@@ -1,3 +1,45 @@
 public class Hotel {
 	
+	public String nameOfHotel;
+	public String locationOfHotel;
+	public ArrayList<Room> rooms;
+	//public boolean aviableForHandic;
+	//public boolean pickUp;
+	//public boolean breakfastIncluded;
+	public Date checkInStart;
+	public Date checkInFinish;
+
+	public Hotel(String nameOfHotel, String locationOfHotel) {
+		this.nameOfHotel = nameOfHotel;
+		this.locationOfHotel = locationOfHotel;
+		//this.checkInStart = checkInStart;
+		//this.checkInFinish = checkInFinish;
+	}
+
+	public String getNameOfHotel() {
+		return nameOfHotel;
+	}
+
+	public String getLocationOfHotel() {
+		return locationOfHotel;
+	}
+
+	/*public Date getCheckInStart() {
+		return checkInStart;
+	}
+
+	public Date getCheckInFinish() {
+		return checkInFinish;
+	}*/
+
+	// Býr til herbergi fyrir þetta hótel
+	public addRoom(int priceForNight, int numberOfAdults, int numberOfChildren, Date[] isBooked) {
+		Room roomTest = new Room(priceForNight, numberOfAdults, numberOfChildren, isBooked);
+		rooms.push(roomTest);
+	}
+
+	// skilar herbergjum fyrir þetta hotel
+	public ArrayList<Room> viewRooms() {
+		return rooms;
+	}
 }
--- SearchTest.java
+++ SearchTest.java
@@ -1,3 +1,42 @@
+import java.util.AraryList;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class SearchTest {
 	private AraryList<Hotels> hotels;
+
+	@Before
+	public void makeIt() {
+		try {
+			MockConnectionDB conn = new MockConnectionDB();
+			hotels = conn.getMatchingHotelsFromDB("");
+		} catch (Exteption e) {
+			System.out.println(e);
+		}
+	}
+
+	@After
+	public void clear() {
+		hotels = null;
+	}
+
+
+	@Test
+	public void testSearchHotelName() {
+		String querySearch = "Grand";
+
+		AraryList<Hotels> testList = hotels;
+		testList.removeIf(hotel -> !hotel.nameOfHotel.contains(querySearch));
+
+		assertNotNull(testList);
+		assertFalse(testList.isEmpty());
+		for (Hotel item : testList) {
+			assert(item.nameOfHotel.contains(querySearch));
+		}
+	}
+
+
 }
